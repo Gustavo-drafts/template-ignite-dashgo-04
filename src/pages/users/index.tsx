db68d6c89de9df6850a08202a7a5daee55aaf5da
gutoo01 <gu.trindadelive@gmail.com>
2022-10-25 15:53:44 -0300

    creating links to navigate on the app

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -1,4 +1,5 @@
 import { Box, Button, Center, Checkbox, Flex, Heading, Icon, Table, Tbody, Td, Text, Th, Thead, Tr, useBreakpointValue } from "@chakra-ui/react";
+import Link from "next/link";
 import { RiAddLine, RiPencilLine } from "react-icons/ri";
 import { Header } from "../../components/Header";
 import { Pagination } from "../../components/Pagination";
@@ -21,21 +22,23 @@ export default function UserList() {
           <Flex mb={8} justify='space-between' align='center'>
             <Heading size='lg' fontWeight='normal'>Usuários</Heading>
 
-            <Button
-              as='a'
-              size='sm'
-              fontSize='sm'
-              colorScheme='pink'
-              leftIcon={<Icon as={RiAddLine} fontSize='20' />}
-            >
-              Criar novo
-            </Button>
+            <Link href='/users/create' passHref>
+              <Button
+                as='a'
+                size='sm'
+                fontSize='sm'
+                colorScheme='pink'
+                leftIcon={<Icon as={RiAddLine} fontSize='20' />}
+              >
+                Criar novo
+              </Button>
+            </Link>
           </Flex>
 
           <Table colorScheme='whiteAlpha'>
             <Thead>
               <Tr>
-                <Th px={['4','4','6']} color='gray.300' width='8'>
+                <Th px={['4', '4', '6']} color='gray.300' width='8'>
                   <Checkbox colorScheme='pink' />
                 </Th>
                 <Th>Usuário</Th>
@@ -45,7 +48,7 @@ export default function UserList() {
             </Thead>
             <Tbody>
               <Tr>
-                <Td px={['4','4','6']}>
+                <Td px={['4', '4', '6']}>
                   <Checkbox colorScheme='pink' />
                 </Td>
                 <Td>
